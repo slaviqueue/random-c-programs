4c54d72d31947d979458f968cd5a0d23c3e78575
slaviqueue <electricsemyon@gmail.com>
2024-06-13 21:26:31 +0300

    add entab and print-data-type-ranges

---
+++ entab/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SPACES_PER_TAB 2
+
+typedef struct {
+  char *input_file_name;
+  char *output_file_name;
+} Arguments;
+
+static Arguments parse_arguments(int argc, char *argv[]);
+static FILE *open_file_safe(char *file, char *mode);
+static void entab(FILE *input, FILE *output);
+
+int main(int argc, char *argv[]) {
+  Arguments args = parse_arguments(argc, argv);
+
+  FILE *input_file = fopen(args.input_file_name, "r");
+  FILE *output_file = fopen(args.output_file_name, "w");
+
+  entab(input_file, output_file);
+
+  fclose(input_file);
+  fclose(output_file);
+}
+
+static Arguments parse_arguments(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <input_file> <output_file>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  return (Arguments){
+      .input_file_name = argv[1],
+      .output_file_name = argv[2],
+  };
+}
+
+static FILE *open_file_safe(char *name, char *mode) {
+  FILE *file = fopen(name, mode);
+
+  if (file == NULL) {
+    perror("fopen");
+    exit(EXIT_FAILURE);
+  }
+
+  return file;
+}
+
+static void entab(FILE *input, FILE *output) {
+  int current_blanks = 0;
+  char current_char;
+
+  while ((current_char = fgetc(input)) != EOF) {
+    if (current_char == ' ') {
+      if (current_blanks++ == SPACES_PER_TAB) {
+        fputc('\t', output);
+        current_blanks = 0;
+      } else {
+        fputc(current_char, output);
+        current_blanks++;
+      }
+    } else {
+      fputc(current_char, output);
+      current_blanks = 0;
+    }
+  }
+}
---
+++ print-data-type-ranges/main.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Yo, here are some data type ranges according to what we've found in "
+         "<limits.h>:\n");
+
+  printf("Signed char: min = %d, max = %d\n", SCHAR_MIN, SCHAR_MAX);
+  printf("Unsigned char: min = 0, max = %d\n", UCHAR_MAX);
+  printf("Short int: min = %d, max = %d\n", SHRT_MIN, SHRT_MAX);
+  printf("Unsigned short int: min = 0, max = %d\n", USHRT_MAX);
+  printf("Regular int: min = %d, max = %d\n", INT_MIN, INT_MAX);
+  printf("Unsigned int: min = 0, max = %ud\n", UINT_MAX);
+  printf("Long int: min = %ld, max = %ld\n", LONG_MIN, LONG_MAX);
+  printf("Unsigned long int: min = 0, max = %lu\n", ULONG_MAX);
+
+  return EXIT_SUCCESS;
+}
