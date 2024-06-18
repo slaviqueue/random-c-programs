7246bfc2659f666d97a13b81bb7c961d12fc1654
slaviqueue <electricsemyon@gmail.com>
2024-06-18 22:35:23 +0300

    remove log from run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,6 @@ fi
 mkdir -p ./build/programs/
 
 source_files=$(ls $program_to_run/*.c | grep -v test.c)
-echo "source_files: $source_files"
 gcc -o ./build/$program_to_run $source_files
 
 ./build/$program_to_run
