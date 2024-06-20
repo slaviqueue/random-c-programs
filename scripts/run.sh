9351be68b0a9191a056a878b8594ea1c322bae26
slaviqueue <electricsemyon@gmail.com>
2024-06-20 23:13:16 +0300

    add an option to pass gcc_flags to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,15 +2,16 @@
 
 set -e
 program_to_run=$1
+gcc_flags=$2
 
 if [ -z "$program_to_run" ]; then
-    echo "Usage: $0 <program_to_run>"
+    echo "usage: $0 <program_to_run>"
     exit 1
 fi
 
 mkdir -p ./build/programs/
 
 source_files=$(ls $program_to_run/*.c | grep -v test.c)
-gcc -o ./build/$program_to_run $source_files
+gcc -o ./build/$program_to_run $source_files $gcc_flags
 
 ./build/$program_to_run
