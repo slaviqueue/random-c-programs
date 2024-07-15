24e3f67cde5e4fae543ceb0a82060265c0bf519b
slaviqueue <electricsemyon@gmail.com>
2024-07-15 14:29:39 +0300

    improve error handling in cat

--- programs/cat/main.c
+++ programs/cat/main.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 
 static void copystr(FILE* input, FILE* output);
-static bool copy_file_to_stdout(char* file_name);
+static bool copy_file_to_stdout(char* program_name, char* file_name);
 
 int main(int argc, char* argv[]) {
+  char* program_name = *argv;
   bool copying_from_stdin = argc == 1;
 
   if (copying_from_stdin)
     copystr(stdin, stdout);
   else
     while (--argc > 0)
-      if (!copy_file_to_stdout(*++argv))
+      if (!copy_file_to_stdout(program_name, *++argv))
         return EXIT_FAILURE;
+
+  if (ferror(stdout)) {
+    fprintf(stderr, "%s: Couldn't write to stdout\n", program_name);
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
@@ -25,11 +31,11 @@ static void copystr(FILE* input, FILE* output) {
     fputc(c, output);
 }
 
-static bool copy_file_to_stdout(char* file_name) {
+static bool copy_file_to_stdout(char* program_name, char* file_name) {
   FILE* input = fopen(file_name, "r");
 
   if (!input) {
-    fprintf(stderr, "Couldn't open the file %s\n", file_name);
+    fprintf(stderr, "%s: Couldn't open the file %s\n", program_name, file_name);
     return false;
   }
 
