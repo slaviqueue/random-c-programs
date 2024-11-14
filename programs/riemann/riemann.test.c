4f5581886ce9111af0e8ec508ad5682f7a1f4751
slaviqueue <electricsemyon@gmail.com>
2024-11-14 15:57:57 +0200

    update riemann test

--- programs/riemann/riemann.test.c
+++ programs/riemann/riemann.test.c
@@ -2,13 +2,12 @@
 #include <criterion/internal/assert.h>
 #include <criterion/internal/test.h>
 #include <math.h>
-#include <stdio.h>
 
 float some_weird_function(float x) {
   return -pow(x, 2) - x;
 }
 
-Test(riemann_calculate, calculates_riemann_sum) {
+Test(riemann_approximate, calculates_riemann_sum) {
   Interval interval = {-1, 2};
   int steps = 5;
   float actual_approximation;
@@ -16,7 +15,6 @@ Test(riemann_calculate, calculates_riemann_sum) {
                                              interval, &actual_approximation);
   float expected_approximation = -441.0 / 100.0;
 
-  printf("%f\n", actual_approximation);
   cr_expect_eq(status, RiemannStatusSuccess);
   cr_expect_float_eq(actual_approximation, expected_approximation, 0.0001);
 }
